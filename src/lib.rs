346e44947ef14f4c8185d965c37a90eb710fe744
Tibor Gáts <tibor.gats@gmail.com>
2016-06-04 18:48:39 +0200

    some clippy settings added

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,16 @@
 //! music = { git = "https://github.com/tiborgats/music" }
 //! ```
 
-#![warn(missing_docs)]
+#![forbid(exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
+        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
+        unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(bad_style, trivial_casts, trivial_numeric_casts, unused_extern_crates,
+        unused_import_braces, unused_qualifications, unused_results)]
+
 //#![feature(question_mark)]
 
 #[cfg(feature = "be-portaudio")]

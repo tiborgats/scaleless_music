09e9c274b8147ce18455448b3a92d3d26f748615
Tibor Gáts <tibor.gats@gmail.com>
2016-07-24 08:43:50 +0200

    Progress struct is based on Timer struct now.

--- src/sound/progress.rs
+++ src/sound/progress.rs
@@ -3,71 +3,36 @@ use std::cell::Cell;
 
 /// Common methods of the Progress types.
 pub trait Progress {
-    /// Simplifies the phase to achieve higher accuracy. It is only used for periodic functions.
-    fn simplify(&self);
+    /// Sets the duration, and restarts the progress.
+    fn set_timing(&self, timing: TimingOption) -> SoundResult<()>;
+
     /// Sets a new period unit, and restarts the progress. Period unit is the amount of phase
     /// change during one period.
     fn set_period_unit(&self, period_unit: SampleCalc);
+
     /// Sets a new initial phase value, and restarts the progress.
     fn set_phase_init(&self, phase: SampleCalc);
+
+    /// Simplifies the phase to achieve higher accuracy. It is only used for periodic functions.
+    fn simplify(&self);
+
     /// Restarts the progress.
     fn restart(&self);
-    /// Returns the final phase value. This phase value will be the last one when the progress
-    /// reaches it's duration.
-    fn get_phase_final(&self) -> SampleCalc;
-}
-
-/// Simple time progress.
-#[derive(Debug, Clone)]
-pub struct ProgressTimeSimple {
-    sample_time: SampleCalc,
-    duration: Cell<SampleCalc>,
-    /// The actual time.
-    time: Cell<SampleCalc>,
-}
-
-impl ProgressTimeSimple {
-    /// Custom constructor with duration.
-    pub fn new(sample_rate: SampleCalc, duration: SampleCalc) -> SoundResult<ProgressTimeSimple> {
-        let sample_time = try!(get_sample_time(sample_rate));
-        if duration <= 0.0 {
-            return Err(Error::PeriodInvalid);
-        }
-        Ok(ProgressTimeSimple {
-            sample_time: sample_time,
-            duration: Cell::new(duration),
-            time: Cell::new(0.0),
-        })
-    }
 
     /// Provides the next phase value, or `Error::ProgressCompleted` if the progress is finished.
-    pub fn next(&self) -> SoundResult<SampleCalc> {
-        self.time.set(self.time.get() + self.sample_time);
-        if self.time.get() >= self.duration.get() {
-            return Err(Error::ProgressCompleted);
-        }
-        Ok(self.time.get())
-    }
-}
+    fn next_by_time(&self) -> SoundResult<SampleCalc>;
 
-impl Progress for ProgressTimeSimple {
-    fn simplify(&self) {}
+    /// Provides the next phase value depending on the actual tempo, or
+    /// `Error::ProgressCompleted` if progress is finished.
+    /// Tempo value is given in beats per second.
+    fn next_by_tempo(&self, tempo: SampleCalc) -> SoundResult<SampleCalc>;
 
-    fn set_period_unit(&self, _period_unit: SampleCalc) {
-        self.restart();
-    }
+    // Returns the final phase value. This phase value will be the last one when the progress
+    // reaches it's duration.
+    // fn get_phase_final(&self) -> SoundResult<SampleCalc>;
 
-    fn set_phase_init(&self, _phase: SampleCalc) {
-        self.restart();
-    }
-
-    fn restart(&self) {
-        self.time.set(0.0);
-    }
-
-    fn get_phase_final(&self) -> SampleCalc {
-        self.duration.get()
-    }
+    /// Returns the actual phase value.
+    fn get_phase(&self) -> SampleCalc;
 }
 
 
@@ -75,8 +40,7 @@ impl Progress for ProgressTimeSimple {
 /// calling `next_phase()`. The whole duration can be divided to periods, for periodic functions.
 #[derive(Debug, Clone)]
 pub struct ProgressTime {
-    sample_time: SampleCalc,
-    duration: Cell<SampleCalc>,
+    timer: Timer,
     /// For periodic functions: the duration of one period.
     period: Cell<SampleCalc>,
     /// The amount of phase change during one period.
@@ -84,54 +48,29 @@ pub struct ProgressTime {
     /// Initial value of `phase`.
     phase_init: Cell<SampleCalc>,
     phase_change: Cell<SampleCalc>,
-    /// The remaining time.
-    remaining_time: Cell<SampleCalc>,
     /// The phase of the progress.
     phase: Cell<SampleCalc>,
 }
 
 impl ProgressTime {
-    /// Custom constructor with duration. The default period is the whole duration.
+    /// Custom constructor with duration. The default duration is the period.
     /// The default period unit is π x 2.
-    pub fn new(sample_rate: SampleCalc, duration: SampleCalc) -> SoundResult<ProgressTime> {
-        let sample_time = try!(get_sample_time(sample_rate));
-        if duration <= 0.0 {
+    pub fn new(sample_rate: SampleCalc, period: SampleCalc) -> SoundResult<ProgressTime> {
+        let timer = try!(Timer::new(sample_rate));
+        if period <= 0.0 {
             return Err(Error::PeriodInvalid);
         }
-        let period = duration;
+        try!(timer.set(TimingOption::TimeConst(period)));
         let period_unit = PI2;
-        let phase_change = (sample_time / period) * period_unit;
+        let phase_change = (timer.get_sample_time() / period) * period_unit;
         Ok(ProgressTime {
-            sample_time: sample_time,
-            duration: Cell::new(duration),
+            timer: timer,
             period: Cell::new(period),
             period_unit: Cell::new(period_unit),
             phase_init: Cell::new(0.0),
             phase_change: Cell::new(phase_change),
-            remaining_time: Cell::new(duration),
             phase: Cell::new(0.0),
         })
-    }
-
-    /// Provides the next phase value, or `Error::ProgressCompleted` if the progress is finished.
-    pub fn next_phase(&self) -> SoundResult<SampleCalc> {
-        self.remaining_time.set(self.remaining_time.get() - self.sample_time);
-        if self.remaining_time.get() <= 0.0 {
-            return Err(Error::ProgressCompleted);
-        }
-        self.phase.set(self.phase.get() + self.phase_change.get());
-        Ok(self.phase.get())
-    }
-
-    /// Sets the duration, and restarts the progress.
-    pub fn set_duration(&self, duration: SampleCalc) -> SoundResult<()> {
-        if duration <= 0.0 {
-            return Err(Error::DurationInvalid);
-        }
-        self.duration.set(duration);
-        self.remaining_time.set(self.duration.get());
-        self.restart();
-        Ok(())
     }
 
     /// Sets a new period, and restarts the progress.
@@ -154,8 +93,10 @@ impl ProgressTime {
 }
 
 impl Progress for ProgressTime {
-    fn simplify(&self) {
-        self.phase.set(self.phase.get() % self.period_unit.get());
+    fn set_timing(&self, timing: TimingOption) -> SoundResult<()> {
+        try!(self.timer.set(timing));
+        self.restart();
+        Ok(())
     }
 
     fn set_period_unit(&self, period_unit: SampleCalc) {
@@ -168,14 +109,41 @@ impl Progress for ProgressTime {
         self.restart();
     }
 
-    fn restart(&self) {
-        self.phase.set(self.phase_init.get());
-        self.remaining_time.set(self.duration.get());
-        self.phase_change.set((self.sample_time / self.period.get()) * self.period_unit.get());
+    fn simplify(&self) {
+        self.phase.set(self.phase.get() % self.period_unit.get());
     }
 
-    fn get_phase_final(&self) -> SampleCalc {
-        self.phase_init.get() + ((self.duration.get() / self.period.get()) * self.period_unit.get())
+    fn restart(&self) {
+        self.timer.restart();
+        self.phase.set(self.phase_init.get());
+        self.phase_change
+            .set((self.timer.get_sample_time() / self.period.get()) * self.period_unit.get());
+    }
+
+    fn next_by_time(&self) -> SoundResult<SampleCalc> {
+        try!(self.timer.next_by_time());
+        self.phase.set(self.phase.get() + self.phase_change.get());
+        Ok(self.phase.get())
+    }
+
+    /// Note: it means: the duration is tempo dependent, but the phase change is time dependent.
+    fn next_by_tempo(&self, tempo: SampleCalc) -> SoundResult<SampleCalc> {
+        try!(self.timer.next_by_tempo(tempo));
+        self.phase.set(self.phase.get() + self.phase_change.get());
+        Ok(self.phase.get())
+    }
+
+    // fn get_phase_final(&self) -> SoundResult<SampleCalc> {
+    // match self.duration.get() {
+    // Some(d) => {
+    // Ok(self.phase_init.get() + ((d / self.period.get()) * self.period_unit.get()))
+    // }
+    // None => Err(Error::DurationInvalid),
+    // }
+    // }
+
+    fn get_phase(&self) -> SampleCalc {
+        self.phase.get()
     }
 }
 
@@ -183,9 +151,7 @@ impl Progress for ProgressTime {
 /// calling `next_phase()`. The whole duration can be divided to periods, for periodic functions.
 #[derive(Debug, Clone)]
 pub struct ProgressTempo {
-    sample_time: SampleCalc,
-    /// The tempo relative duration, measured in beats.
-    duration: Cell<NoteValue>,
+    timer: Timer,
     /// For periodic functions: the duration of one period in beats.
     period: Cell<NoteValue>,
     /// The amount of phase change during one period.
@@ -193,8 +159,6 @@ pub struct ProgressTempo {
     /// Initial value of `phase`.
     phase_init: Cell<SampleCalc>,
     phase_change: Cell<SampleCalc>,
-    /// The remaining beats.
-    remaining_beats: Cell<SampleCalc>,
     /// The phase of the progress.
     phase: Cell<SampleCalc>,
 }
@@ -202,38 +166,19 @@ pub struct ProgressTempo {
 impl ProgressTempo {
     /// Custom constructor. The default period is the whole duration.
     /// The default period unit is π x 2.
-    pub fn new(sample_rate: SampleCalc, duration: NoteValue) -> SoundResult<ProgressTempo> {
-        let sample_time = try!(get_sample_time(sample_rate));
-        let period = duration;
+    pub fn new(sample_rate: SampleCalc, period: NoteValue) -> SoundResult<ProgressTempo> {
+        let timer = try!(Timer::new(sample_rate));
+        try!(timer.set(TimingOption::Tempo(period)));
         let period_unit = PI2;
-        let phase_change = sample_time * period.get_notes_per_beat() * period_unit;
+        let phase_change = timer.get_sample_time() * period.get_notes_per_beat() * period_unit;
         Ok(ProgressTempo {
-            sample_time: sample_time,
-            duration: Cell::new(duration),
+            timer: timer,
             period: Cell::new(period),
             period_unit: Cell::new(period_unit),
             phase_init: Cell::new(0.0),
             phase_change: Cell::new(phase_change),
-            remaining_beats: Cell::new(duration.get_duration_in_beats()),
             phase: Cell::new(0.0),
         })
-    }
-
-    /// Provides the next phase value depending on the actual tempo, or
-    /// `Error::ProgressCompleted` if progress is finished.
-    pub fn next_phase(&self, beats_per_second: SampleCalc) -> SoundResult<SampleCalc> {
-        self.remaining_beats.set(self.remaining_beats.get() - self.sample_time * beats_per_second);
-        if self.remaining_beats.get() <= 0.0 {
-            return Err(Error::ProgressCompleted);
-        }
-        self.phase.set(self.phase.get() + self.phase_change.get() * beats_per_second);
-        Ok(self.phase.get())
-    }
-
-    /// Sets the duration in tempo beats, and restarts the progress.
-    pub fn set_duration(&self, duration: NoteValue) {
-        self.duration.set(duration);
-        self.restart();
     }
 
     /// Sets a new period in tempo beats, and restarts the progress.
@@ -244,8 +189,10 @@ impl ProgressTempo {
 }
 
 impl Progress for ProgressTempo {
-    fn simplify(&self) {
-        self.phase.set(self.phase.get() % self.period_unit.get());
+    fn set_timing(&self, timing: TimingOption) -> SoundResult<()> {
+        try!(self.timer.set(timing));
+        self.restart();
+        Ok(())
     }
 
     fn set_period_unit(&self, period_unit: SampleCalc) {
@@ -258,17 +205,40 @@ impl Progress for ProgressTempo {
         self.restart();
     }
 
-    fn restart(&self) {
-        self.phase.set(self.phase_init.get());
-        self.remaining_beats.set(self.duration.get().get_duration_in_beats());
-        self.phase_change.set(self.sample_time * self.period.get().get_notes_per_beat() *
-                              self.period_unit.get());
+    fn simplify(&self) {
+        self.phase.set(self.phase.get() % self.period_unit.get());
     }
 
-    fn get_phase_final(&self) -> SampleCalc {
-        self.phase_init.get() +
-        (self.duration.get().get_duration_in_beats() * self.period.get().get_notes_per_beat() *
-         self.period_unit.get())
+    fn restart(&self) {
+        self.timer.restart();
+        self.phase.set(self.phase_init.get());
+        self.phase_change
+            .set(self.timer.get_sample_time() * self.period.get().get_notes_per_beat() *
+                 self.period_unit.get());
+    }
+
+    fn next_by_time(&self) -> SoundResult<SampleCalc> {
+        // TODO: investigate if we need this method implementation at all
+        // try!(self.timer.next_by_time());
+        // self.phase.set(self.phase.get() + self.phase_change.get() * tempo);
+        // Ok(self.phase.get())
+        Err(Error::ProgressInvalid)
+    }
+
+    fn next_by_tempo(&self, tempo: SampleCalc) -> SoundResult<SampleCalc> {
+        try!(self.timer.next_by_tempo(tempo));
+        self.phase.set(self.phase.get() + self.phase_change.get() * tempo);
+        Ok(self.phase.get())
+    }
+
+    // fn get_phase_final(&self) -> SoundResult<SampleCalc> {
+    // Ok(self.phase_init.get() +
+    // (self.duration.get().get_duration_in_beats() * self.period.get().get_notes_per_beat() *
+    // self.period_unit.get()))
+    // }
+
+    fn get_phase(&self) -> SampleCalc {
+        self.phase.get()
     }
 }
 
@@ -282,10 +252,10 @@ pub enum ProgressOption {
 }
 
 impl Progress for ProgressOption {
-    fn simplify(&self) {
+    fn set_timing(&self, timing: TimingOption) -> SoundResult<()> {
         match *self {
-            ProgressOption::Time(ref p) => p.simplify(),
-            ProgressOption::Tempo(ref p) => p.simplify(),
+            ProgressOption::Time(ref p) => p.set_timing(timing),
+            ProgressOption::Tempo(ref p) => p.set_timing(timing),
         }
     }
 
@@ -303,6 +273,13 @@ impl Progress for ProgressOption {
         }
     }
 
+    fn simplify(&self) {
+        match *self {
+            ProgressOption::Time(ref p) => p.simplify(),
+            ProgressOption::Tempo(ref p) => p.simplify(),
+        }
+    }
+
     fn restart(&self) {
         match *self {
             ProgressOption::Time(ref p) => p.restart(),
@@ -310,10 +287,31 @@ impl Progress for ProgressOption {
         }
     }
 
-    fn get_phase_final(&self) -> SampleCalc {
+    fn next_by_time(&self) -> SoundResult<SampleCalc> {
         match *self {
-            ProgressOption::Time(ref p) => p.get_phase_final(),
-            ProgressOption::Tempo(ref p) => p.get_phase_final(),
+            ProgressOption::Time(ref p) => p.next_by_time(),
+            ProgressOption::Tempo(ref p) => p.next_by_time(),
+        }
+    }
+
+    fn next_by_tempo(&self, tempo: SampleCalc) -> SoundResult<SampleCalc> {
+        match *self {
+            ProgressOption::Time(ref p) => p.next_by_tempo(tempo),
+            ProgressOption::Tempo(ref p) => p.next_by_tempo(tempo),
+        }
+    }
+
+    // fn get_phase_final(&self) -> SoundResult<SampleCalc> {
+    // match *self {
+    // ProgressOption::Time(ref p) => p.get_phase_final(),
+    // ProgressOption::Tempo(ref p) => p.get_phase_final(),
+    // }
+    // }
+
+    fn get_phase(&self) -> SampleCalc {
+        match *self {
+            ProgressOption::Time(ref p) => p.get_phase(),
+            ProgressOption::Tempo(ref p) => p.get_phase(),
         }
     }
 }
